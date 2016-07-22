0605ccbd84e0125d2bf194ed3ae505b5998dc51e
Eduardo Oviedo Blanco <Eduardo Oviedo Blanco>
2016-07-21 18:16:16 -0600

    Ejemplo de comentarios

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
+    /* Mi primer programa en C#
+     * Imprime Hola Mundo 
+     */
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
             Console.WriteLine("Hello World");
             Console.WriteLine("Press any key to exit");
+            // Lee una tecla
             Console.ReadKey();
         }
     }
